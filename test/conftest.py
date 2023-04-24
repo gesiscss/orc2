9d98857d0dd144c2d29bcf78e467264a7082cdc2
Raniere Silva <Raniere.CostadaSilva@gesis.com>
2023-04-24 13:15:41 +0200

    Improve test script

--- test/conftest.py
+++ test/conftest.py
@@ -1,25 +1,29 @@
 """
 Sharing scope for test
 
-Read https://docs.pytest.org/en/6.2.x/fixture.html#scope-sharing-fixtures-across-classes-modules-packages-or-session for more information.
+Read https://docs.pytest.org/en/6.2.x/fixture.html
+for more information.
 """
 
 import pytest
 
 
 def pytest_addoption(parser):
+    """Add extra options to command line parser."""
     parser.addoption(
-        "--binder-url", help="Fully qualified URL to the binder installation"
+        "--binder-url", help="Fully qualified URL to the Binder installation", required=True
     )
 
-    parser.addoption("--hub-url", help="Fully qualified URL to the hub installation")
+    parser.addoption("--hub-url", help="Fully qualified URL to the JupyterHub installation", required=True)
 
 
 @pytest.fixture
 def binder_url(request):
+    """Get Binder URL from command line arguments."""
     return request.config.getoption("--binder-url").rstrip("/")
 
 
 @pytest.fixture
 def hub_url(request):
+    """Get Jupyter Hub URL from command line arguments."""
     return request.config.getoption("--hub-url").rstrip("/")
--- test/test_http.py
+++ test/test_http.py
@@ -1,28 +1,29 @@
 """Basic HTTP tests to make sure things are running"""
 import pprint
 
-import pytest
 import requests
+
+TIMEOUT = 30
 
 
 def test_binder_up(binder_url):
     """
     Binder Hub URL is up & returning sensible text
     """
-    resp = requests.get(binder_url)
+    resp = requests.get(binder_url, timeout=TIMEOUT)
     assert resp.status_code == 200
     assert "GitHub" in resp.text
 
 
 def test_hub_health(hub_url):
     """check JupyterHubHub health endpoint"""
-    resp = requests.get(hub_url + "/hub/api/health")
+    resp = requests.get(hub_url + "/hub/api/health", timeout=TIMEOUT)
     print(resp.text)
     assert resp.status_code == 200
 
 
 def test_binder_health(binder_url):
     """check BinderHub health endpoint"""
-    resp = requests.get(binder_url + "/health")
+    resp = requests.get(binder_url + "/health", timeout=TIMEOUT)
     pprint.pprint(resp.json())
     assert resp.status_code == 200
