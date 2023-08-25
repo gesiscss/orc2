ee2ed48a5d3bf127ed54046beab86850776f2005
Raniere Silva <Raniere.CostadaSilva@gesis.org>
2023-08-25 18:05:39 +0200

    Add wrap to staticjinja
    and change GitLab CI to use wrap for staticjinja

---
+++ pages/staticjinja-gesis-notebooks.py
@@ -0,0 +1,31 @@
+import argparse
+
+from staticjinja import Site
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Wrap around staticjinja for GESIS Notebooks.')
+    
+    parser.add_argument('--srcpath', default='.')
+    parser.add_argument('--outpath', default='.')
+    
+    parser.add_argument('--production', action='store_true')
+    parser.add_argument('--stage', action='store_true')
+    parser.add_argument('--local', action='store_true')
+
+    args = parser.parse_args()
+
+    context = {}
+
+    if args.local:
+        context['gesis_notebooks_https'] = '/'
+    if args.stage:
+        context['gesis_notebooks_https'] = 'https://notebooks-test.gesis.org/'
+    if args.production:
+        context['gesis_notebooks_https'] = 'https://notebooks.gesis.org/'
+    
+    site = Site.make_site(
+        searchpath=args.srcpath,
+        outpath=args.outpath,
+        env_globals=context
+    )
+    site.render()
